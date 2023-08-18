4fbb67a64c896f074dd24d951f354865dd950b50
kronnox10 <134155626+kronnox10@users.noreply.github.com>
2023-08-18 13:01:33 -0500

    Add files via upload

---
+++ Js/EditarTable.js
@@ -0,0 +1,174 @@
+    //new DataTable('#tablacontacs');
+    $(document).ready(function () {
+    $('#tablacontacs').DataTable({
+        "language": {
+            "lengthMenu": "Mostrar _MENU_ registros",
+            "zeroRecords": "No se encontró resultado",
+            "info": "Mostrar _START_ de _END_ de _TOTAL_ registros",
+            "infoEmpty": "Mostrar 0 de 0 de 0 registros",
+            "infoFiltered": "(filtrado de _MAX_ registros en total)",
+            "search": "Buscar:",
+            "paginate": {
+                "first": "Primero",
+                "last": "Último",
+                "next": "Siguiente",
+                "previous": "Anterior"
+            }
+        },"searching": true // Habilitar búsqueda
+    });
+});
+
+ //new DataTable('#tablacontacs');
+ $(document).ready(function () {
+    $('#tablacontacs2').DataTable({
+        "language": {
+            "lengthMenu": "Mostrar _MENU_ registros",
+            "zeroRecords": "No se encontró resultado",
+            "info": "Mostrar _START_ de _END_ de _TOTAL_ registros",
+            "infoEmpty": "Mostrar 0 de 0 de 0 registros",
+            "infoFiltered": "(filtrado de _MAX_ registros en total)",
+            "search": "Buscar:",
+            "paginate": {
+                "first": "Primero",
+                "last": "Último",
+                "next": "Siguiente",
+                "previous": "Anterior"
+            }
+        },"searching": true // Habilitar búsqueda
+    });
+});
+
+
+$(document).ready(function () {
+    $('#tablaentrenador').DataTable({
+        "language": {
+            "lengthMenu": "Mostrar _MENU_ registros",
+            "zeroRecords": "No se encontró resultado",
+            "info": "Mostrar _START_ de _END_ de _TOTAL_ registros",
+            "infoEmpty": "Mostrar 0 de 0 de 0 registros",
+            "infoFiltered": "(filtrado de _MAX_ registros en total)",
+            "search": "Buscar:",
+            "paginate": {
+                "first": "Primero",
+                "last": "Último",
+                "next": "Siguiente",
+                "previous": "Anterior"
+            }
+        },"searching": true // Habilitar búsqueda
+    });
+});
+
+
+$(document).ready(function () {
+    $('#tablaentrenador2').DataTable({
+        "language": {
+            "lengthMenu": "Mostrar _MENU_ registros",
+            "zeroRecords": "No se encontró resultado",
+            "info": "Mostrar _START_ de _END_ de _TOTAL_ registros",
+            "infoEmpty": "Mostrar 0 de 0 de 0 registros",
+            "infoFiltered": "(filtrado de _MAX_ registros en total)",
+            "search": "Buscar:",
+            "paginate": {
+                "first": "Primero",
+                "last": "Último",
+                "next": "Siguiente",
+                "previous": "Anterior"
+            }
+        },"searching": true // Habilitar búsqueda
+    });
+});
+
+
+$(document).ready(function () {
+  $('#tablaSesion').DataTable({
+      "language": {
+          "lengthMenu": "Mostrar _MENU_ registros",
+          "zeroRecords": "No se encontró resultado",
+          "info": "Mostrar _START_ de _END_ de _TOTAL_ registros",
+          "infoEmpty": "Mostrar 0 de 0 de 0 registros",
+          "infoFiltered": "(filtrado de _MAX_ registros en total)",
+          "search": "Buscar:",
+          "paginate": {
+              "first": "Primero",
+              "last": "Último",
+              "next": "Siguiente",
+              "previous": "Anterior"
+          }
+      },"searching": true // Habilitar búsqueda
+  });
+});
+
+
+$(document).ready(function () {
+    $('#tablaAceptada').DataTable({
+        "language": {
+            "lengthMenu": "Mostrar _MENU_ registros",
+            "zeroRecords": "No se encontró resultado",
+            "info": "Mostrar _START_ de _END_ de _TOTAL_ registros",
+            "infoEmpty": "Mostrar 0 de 0 de 0 registros",
+            "infoFiltered": "(filtrado de _MAX_ registros en total)",
+            "search": "Buscar:",
+            "paginate": {
+                "first": "Primero",
+                "last": "Último",
+                "next": "Siguiente",
+                "previous": "Anterior"
+            }
+        },"searching": true // Habilitar búsqueda
+    });
+  });
+  
+  
+  $(document).ready(function () {
+    $('#tablaSolicitud').DataTable({
+        "language": {
+            "lengthMenu": "Mostrar _MENU_ registros",
+            "zeroRecords": "No se encontró resultado",
+            "info": "Mostrar _START_ de _END_ de _TOTAL_ registros",
+            "infoEmpty": "Mostrar 0 de 0 de 0 registros",
+            "infoFiltered": "(filtrado de _MAX_ registros en total)",
+            "search": "Buscar:",
+            "paginate": {
+                "first": "Primero",
+                "last": "Último",
+                "next": "Siguiente",
+                "previous": "Anterior"
+            }
+        },"searching": true // Habilitar búsqueda
+    });
+  });
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*window.addEventListener('DOMContentLoaded', event => {
+    // Simple-DataTables
+    // https://github.com/fiduswriter/Simple-DataTables/wiki
+
+    const datatablesSimple = document.getElementById('tablacontacs');
+    if (datatablesSimple) {
+        new simpleDatatables.DataTable(datatablesSimple);
+    }
+});
+
+
+  document.addEventListener('DOMContentLoaded', function () {
+    new simpleDatatables.DataTable('#tablacontacs', {
+      perPage: 5, // Establecer el número de entradas por página
+      searchable: false, // Para deshabilitar la búsqueda, ya que mencionaste que no quieres que se muestren todas las entradas en la tabla.
+    });
+  });
+
+*/
---
+++ Js/registro_rutina.js
@@ -0,0 +1,38 @@
+function getrutina() {
+    miStorage = window.localStorage;
+    let usuarioid = JSON.parse(miStorage.getItem('usuario'));
+    ide = usuarioid.id
+    console.log(ide)
+    //xd
+    const vdescripcion = document.getElementById("descripcion").value;
+    const vurl = document.getElementById("url").value;
+    const tipo = document.getElementById("rutina").value;
+    const vid = ide
+
+    axios({
+        //SI
+        method: 'POST',
+        url: 'http://127.0.0.1:6122/add_rutina',
+        data: {
+            id_entrenador: vid,
+            descripcion: vdescripcion,
+            tipo_rutina: tipo,
+            URLVIDEO: vurl
+
+        },
+    }).then(function (response) {
+        if (response.data[0].Informacion == "Ya_existe") {
+            alert("Ya existe una rutina con ese nombre")
+        } else {
+            alert("rutina agregada ")
+            document.getElementById("descripcion").value = "";
+            document.getElementById("rutina").value = "";
+            document.getElementById("url").value = "";
+        }
+
+
+    }).catch(err => console.log('Error: ', err))
+
+}
+
+//----- pues si
